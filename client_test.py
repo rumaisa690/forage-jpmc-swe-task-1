932cc25bb825d740f20e793f525096e90787e12f
rumaisa690 <rumaisa167@gmail.com>
2024-07-03 17:07:30 -0400

    added unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,31 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
 
-  """ ------------ Add more unit tests ------------ """
+  """ ------------ Adding more unit tests ------------ """
+  def test_calculateCorrectRation(self):
+    #  test to make sure getRatio calculates the correct ratio of two prices passed in
+    prices = [(120.68, 121.48), (119.2, 117.87)]
+
+    for a, b in prices:
+       self.assertEqual(getRatio(a, b), (a / b))
+  
+  def test_negativePrices(self):
+  # making sure getRatio can handle negative prices:
+    prices = [(-120.68, 121.48), (-119.2, 117.87)]
+
+    for a, b in prices:
+       self.assertEqual(getRatio(a, b), (a / b))
+  
+
+  def test_divisionByZero(self):
+    #  making sure getRatio correctly handles division by zero by returning None
+    self.assertIsNone(getRatio(120.68, 0))
+  
+
 
 
 
